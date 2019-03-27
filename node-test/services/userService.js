903b7cd418ba7a3427dfd6027f1689a6915c3133
Michael Chang <jaeminchang620@gmail.com>
2019-03-27 10:24:05 -0700

    hello

---
+++ node-test/services/userService.js
@@ -0,0 +1,51 @@
+const { poolPromise } = require("../db");
+const sql = require("mssql");
+
+const insertUser = data => {
+  return new Promise((resolve, reject) => {
+    return poolPromise
+      .then(pool => {
+        pool
+          .request()
+          .input("UserName", sql.NVARCHAR(50), data.userName)
+          .input("Password", sql.NVARCHAR(50), data.password)
+          .output("Id", sql.Int)
+          .execute("dbo.Insert_User", (err, response) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            resolve(response.output);
+          });
+      })
+      .then(sql.close)
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+const selectUser = data => {
+  return new Promise((resolve, reject) => {
+    return poolPromise
+      .then(pool => {
+        pool
+          .request()
+          .input("UserName", sql.NVARCHAR(50), data.userName)
+          .input("Password", sql.NVARCHAR(50), data.password)
+          .execute("dbo.Select_User", (err, response) => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            resolve(response);
+          });
+      })
+      .then(sql.close)
+      .catch(err => {
+        reject(err);
+      });
+  });
+};
+
+module.exports = { insertUser, selectUser };
